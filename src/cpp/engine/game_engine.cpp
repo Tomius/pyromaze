2b9e8ccfeca3951f118fb992915564c0e39c1fbc
Tamás Csala <icyplusplus@gmail.com>
2016-11-28 16:14:52 +0100

    Fixed MSVC compiler errors and some warnings.

--- src/cpp/engine/game_engine.cpp
+++ src/cpp/engine/game_engine.cpp
@@ -7,8 +7,8 @@
 
 #include "./game_engine.hpp"
 
-void DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
-                   GLsizei length, const GLchar *message, const void *userParam) {
+void APIENTRY DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                            GLsizei length, const GLchar *message, const void *userParam) {
   if (severity != GL_DEBUG_SEVERITY_NOTIFICATION) {
     std::cout << message << std::endl;
   }
--- src/cpp/main_scene.cpp
+++ src/cpp/main_scene.cpp
@@ -36,7 +36,7 @@ MainScene::MainScene(GLFWwindow* window, engine::ShaderManager* shader_manager)
     gl::Uniform<int>(prog, "uPointLightCount") = pos_light_count;
   });
 
-  AddLightSource({LightSource::Type::kDirectional, glm::vec3{1.0}, glm::vec3{0.03}});
+  AddLightSource({LightSource::Type::kDirectional, glm::vec3{1.0f}, glm::vec3{0.03f}});
 
   AddComponent<Skybox>("src/resource/skybox.png");
 
